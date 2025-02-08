cdefc4f9a8f7bd3157b380726fcc9a8f870d23d0
ShayanEs <shayaneshteharipour@gmail.com>
2025-02-08 21:22:37 +0330

    formatted using black, uncommented uncomplete code

--- rank_bm25.py
+++ rank_bm25.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import math
-import numpy as np
 from multiprocessing import Pool, cpu_count
+
+import numpy as np
 
 """
 All of these algorithms have been taken from the paper:
@@ -43,7 +44,7 @@ class BM25:
 
             for word, freq in frequencies.items():
                 try:
-                    nd[word]+=1
+                    nd[word] += 1
                 except KeyError:
                     nd[word] = 1
 
@@ -68,7 +69,9 @@ class BM25:
 
     def get_top_n(self, query, documents, n=5):
 
-        assert self.corpus_size == len(documents), "The documents given don't match the index corpus!"
+        assert self.corpus_size == len(
+            documents
+        ), "The documents given don't match the index corpus!"
 
         scores = self.get_scores(query)
         top_n = np.argsort(scores)[::-1][:n]
@@ -116,8 +119,11 @@ class BM25Okapi(BM25):
         doc_len = np.array(self.doc_len)
         for q in query:
             q_freq = np.array([(doc.get(q) or 0) for doc in self.doc_freqs])
-            score += (self.idf.get(q) or 0) * (q_freq * (self.k1 + 1) /
-                                               (q_freq + self.k1 * (1 - self.b + self.b * doc_len / self.avgdl)))
+            score += (self.idf.get(q) or 0) * (
+                q_freq
+                * (self.k1 + 1)
+                / (q_freq + self.k1 * (1 - self.b + self.b * doc_len / self.avgdl))
+            )
         return score
 
     def get_batch_scores(self, query, doc_ids):
@@ -129,8 +135,11 @@ class BM25Okapi(BM25):
         doc_len = np.array(self.doc_len)[doc_ids]
         for q in query:
             q_freq = np.array([(self.doc_freqs[di].get(q) or 0) for di in doc_ids])
-            score += (self.idf.get(q) or 0) * (q_freq * (self.k1 + 1) /
-                                               (q_freq + self.k1 * (1 - self.b + self.b * doc_len / self.avgdl)))
+            score += (self.idf.get(q) or 0) * (
+                q_freq
+                * (self.k1 + 1)
+                / (q_freq + self.k1 * (1 - self.b + self.b * doc_len / self.avgdl))
+            )
         return score.tolist()
 
 
@@ -153,8 +162,12 @@ class BM25L(BM25):
         for q in query:
             q_freq = np.array([(doc.get(q) or 0) for doc in self.doc_freqs])
             ctd = q_freq / (1 - self.b + self.b * doc_len / self.avgdl)
-            score += (self.idf.get(q) or 0) * (self.k1 + 1) * (ctd + self.delta) / \
-                     (self.k1 + ctd + self.delta)
+            score += (
+                (self.idf.get(q) or 0)
+                * (self.k1 + 1)
+                * (ctd + self.delta)
+                / (self.k1 + ctd + self.delta)
+            )
         return score
 
     def get_batch_scores(self, query, doc_ids):
@@ -167,8 +180,12 @@ class BM25L(BM25):
         for q in query:
             q_freq = np.array([(self.doc_freqs[di].get(q) or 0) for di in doc_ids])
             ctd = q_freq / (1 - self.b + self.b * doc_len / self.avgdl)
-            score += (self.idf.get(q) or 0) * (self.k1 + 1) * (ctd + self.delta) / \
-                     (self.k1 + ctd + self.delta)
+            score += (
+                (self.idf.get(q) or 0)
+                * (self.k1 + 1)
+                * (ctd + self.delta)
+                / (self.k1 + ctd + self.delta)
+            )
         return score.tolist()
 
 
@@ -190,8 +207,11 @@ class BM25Plus(BM25):
         doc_len = np.array(self.doc_len)
         for q in query:
             q_freq = np.array([(doc.get(q) or 0) for doc in self.doc_freqs])
-            score += (self.idf.get(q) or 0) * (self.delta + (q_freq * (self.k1 + 1)) /
-                                               (self.k1 * (1 - self.b + self.b * doc_len / self.avgdl) + q_freq))
+            score += (self.idf.get(q) or 0) * (
+                self.delta
+                + (q_freq * (self.k1 + 1))
+                / (self.k1 * (1 - self.b + self.b * doc_len / self.avgdl) + q_freq)
+            )
         return score
 
     def get_batch_scores(self, query, doc_ids):
@@ -203,55 +223,65 @@ class BM25Plus(BM25):
         doc_len = np.array(self.doc_len)[doc_ids]
         for q in query:
             q_freq = np.array([(self.doc_freqs[di].get(q) or 0) for di in doc_ids])
-            score += (self.idf.get(q) or 0) * (self.delta + (q_freq * (self.k1 + 1)) /
-                                               (self.k1 * (1 - self.b + self.b * doc_len / self.avgdl) + q_freq))
+            score += (self.idf.get(q) or 0) * (
+                self.delta
+                + (q_freq * (self.k1 + 1))
+                / (self.k1 * (1 - self.b + self.b * doc_len / self.avgdl) + q_freq)
+            )
         return score.tolist()
 
 
 # BM25Adpt and BM25T are a bit more complicated than the previous algorithms here. Here a term-specific k1
 # parameter is calculated before scoring is done
 
-# class BM25Adpt(BM25):
-#     def __init__(self, corpus, k1=1.5, b=0.75, delta=1):
-#         # Algorithm specific parameters
-#         self.k1 = k1
-#         self.b = b
-#         self.delta = delta
-#         super().__init__(corpus)
-#
-#     def _calc_idf(self, nd):
-#         for word, freq in nd.items():
-#             idf = math.log((self.corpus_size + 1) / freq)
-#             self.idf[word] = idf
-#
-#     def get_scores(self, query):
-#         score = np.zeros(self.corpus_size)
-#         doc_len = np.array(self.doc_len)
-#         for q in query:
-#             q_freq = np.array([(doc.get(q) or 0) for doc in self.doc_freqs])
-#             score += (self.idf.get(q) or 0) * (self.delta + (q_freq * (self.k1 + 1)) /
-#                                                (self.k1 * (1 - self.b + self.b * doc_len / self.avgdl) + q_freq))
-#         return score
-#
-#
-# class BM25T(BM25):
-#     def __init__(self, corpus, k1=1.5, b=0.75, delta=1):
-#         # Algorithm specific parameters
-#         self.k1 = k1
-#         self.b = b
-#         self.delta = delta
-#         super().__init__(corpus)
-#
-#     def _calc_idf(self, nd):
-#         for word, freq in nd.items():
-#             idf = math.log((self.corpus_size + 1) / freq)
-#             self.idf[word] = idf
-#
-#     def get_scores(self, query):
-#         score = np.zeros(self.corpus_size)
-#         doc_len = np.array(self.doc_len)
-#         for q in query:
-#             q_freq = np.array([(doc.get(q) or 0) for doc in self.doc_freqs])
-#             score += (self.idf.get(q) or 0) * (self.delta + (q_freq * (self.k1 + 1)) /
-#                                                (self.k1 * (1 - self.b + self.b * doc_len / self.avgdl) + q_freq))
-#         return score
+
+class BM25Adpt(BM25):
+    def __init__(self, corpus, k1=1.5, b=0.75, delta=1):
+        # Algorithm specific parameters
+        self.k1 = k1
+        self.b = b
+        self.delta = delta
+        super().__init__(corpus)
+
+    def _calc_idf(self, nd):
+        for word, freq in nd.items():
+            idf = math.log((self.corpus_size + 1) / freq)
+            self.idf[word] = idf
+
+    def get_scores(self, query):
+        score = np.zeros(self.corpus_size)
+        doc_len = np.array(self.doc_len)
+        for q in query:
+            q_freq = np.array([(doc.get(q) or 0) for doc in self.doc_freqs])
+            score += (self.idf.get(q) or 0) * (
+                self.delta
+                + (q_freq * (self.k1 + 1))
+                / (self.k1 * (1 - self.b + self.b * doc_len / self.avgdl) + q_freq)
+            )
+        return score
+
+
+class BM25T(BM25):
+    def __init__(self, corpus, k1=1.5, b=0.75, delta=1):
+        # Algorithm specific parameters
+        self.k1 = k1
+        self.b = b
+        self.delta = delta
+        super().__init__(corpus)
+
+    def _calc_idf(self, nd):
+        for word, freq in nd.items():
+            idf = math.log((self.corpus_size + 1) / freq)
+            self.idf[word] = idf
+
+    def get_scores(self, query):
+        score = np.zeros(self.corpus_size)
+        doc_len = np.array(self.doc_len)
+        for q in query:
+            q_freq = np.array([(doc.get(q) or 0) for doc in self.doc_freqs])
+            score += (self.idf.get(q) or 0) * (
+                self.delta
+                + (q_freq * (self.k1 + 1))
+                / (self.k1 * (1 - self.b + self.b * doc_len / self.avgdl) + q_freq)
+            )
+        return score
